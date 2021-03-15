d1eb6ecf3a812165de1c9526721d00fa4e2b59a9
Márcio Bortolini <marciobds@live.it>
2021-03-15 14:48:47 -0300

    Deprecetions

--- app/decorators/models/solidus_virtual_gift_card/spree/store_credit_category_decorator.rb
+++ app/decorators/models/solidus_virtual_gift_card/spree/store_credit_category_decorator.rb
@@ -1,4 +1,13 @@
-# frozen_string_literal: true
+module SolidusVirtualGiftCard
+  module Spree
+    module StoreCreditCategoryDecorator
+      GIFT_CARD_CATEGORY_NAME = 'Gift Card'
 
-Spree::StoreCreditCategory::GIFT_CARD_CATEGORY_NAME = 'Gift Card'
-Spree::StoreCreditCategory.non_expiring_credit_types |= [Spree::StoreCreditCategory::GIFT_CARD_CATEGORY_NAME]
+      def self.prepended(base)
+        base.non_expiring_credit_types |= [GIFT_CARD_CATEGORY_NAME]
+      end
+
+      ::Spree::StoreCreditCategory.prepend self
+    end
+  end
+end
